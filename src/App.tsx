9afa1685264a24316eee6f22af8167ff21e5c462
Akhmadjon-dev <info.ahmadjon@gmail.com>
2021-06-20 18:48:57 +0500

    structured

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
 
-function App() {
+const App = () => {
+
+  const startTrivia = async () => {
+
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+  }
+
+  const nextQuestion =  () =>{
+
+  }
+
   return (
     <div className="quiz">
-      Quiz 
-
-
-
+      <h1 className="quiz__title">MyQuiz</h1>
+    <button onClick={startTrivia} className="quiz__start">
+      start
+    </button>
+    <p className="quiz__score">
+      Score: 
+    </p>
+    <p className="quiz__loader">Loading Questions ...</p>
     </div>
   );
 }
---
+++ src/components/QuestionCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const QuestionCard = () => {
+    return (
+        <div>
+            quesiton card
+        </div>
+    )
+}
+
+export default QuestionCard
