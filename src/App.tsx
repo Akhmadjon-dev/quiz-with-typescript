85f60d34c94a1b37c2cff3dea0a5b289185d92b6
Akhmadjon-dev <info.ahmadjon@gmail.com>
2021-06-21 08:57:39 +0500

    styled body

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import React, { useState } from 'react';
-import QuestionCard from './components/QuestionCard';
-import {fetchQuizQuestions} from './API';
-import {Difficulty, QuestionState} from './API';
-
+import React, { useState } from "react";
+import QuestionCard from "./components/QuestionCard";
+import { fetchQuizQuestions } from "./API";
+import { Difficulty, QuestionState } from "./API";
+import { GlobalStyle, Wrapper } from "./style/App.styles";
 
 export type AnswerObject = {
   question: string;
   answer: string;
   correct: boolean;
   correctAnswer: string;
-}
+};
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
-
   const [loading, setLoading] = useState(false);
   const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
@@ -27,85 +26,83 @@ const App = () => {
     setGameOver(false);
 
     try {
-      const newQuestions = await fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY);
+      const newQuestions = await fetchQuizQuestions(
+        TOTAL_QUESTIONS,
+        Difficulty.EASY
+      );
       setQuestions(newQuestions);
       setScore(0);
       setUserAnswers([]);
-      setNumber(0)
-      console.log(newQuestions)
+      setNumber(0);
+      console.log(newQuestions);
       setLoading(false);
     } catch (error) {
-      console.log(error)
+      console.log(error);
       setLoading(false);
     }
-
-  }
+  };
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-    if(!gameOver){
+    if (!gameOver) {
       const answer = e.currentTarget.value;
       const correct = questions[number].correct_answer === answer;
-      if(correct)setScore(prev => prev+1);
+      if (correct) setScore((prev) => prev + 1);
 
       const answerObj = {
         question: questions[number].question,
         answer,
         correct,
-        correctAnswer: questions[number].correct_answer
-      }
-      setUserAnswers(prev => [...prev, answerObj])
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswers((prev) => [...prev, answerObj]);
     }
-  }
+  };
 
-  const nextQuestion =  () =>{
-
+  const nextQuestion = () => {
     //move on if next exists
-    
-    if(number + 1 === TOTAL_QUESTIONS){
-      setGameOver(true)
-    }else{
-      setNumber(number+1)
-    }
 
-  }
+    if (number + 1 === TOTAL_QUESTIONS) {
+      setGameOver(true);
+    } else {
+      setNumber(number + 1);
+    }
+  };
 
   return (
     <div className="quiz">
-      <h1 className="quiz__title">MyQuiz</h1>
-      {
-        gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+      <GlobalStyle />
 
-        <button onClick={startTrivia} className="quiz__start">
-          Start
-        </button>
-        ): null
-      }
-    {!gameOver ? <p className="quiz__score">
-      Score: 
-    </p>: null}
-    {loading && <p className="quiz__loader">Loading Questions ...</p>}
-    {
-      !loading && !gameOver && (
-        <QuestionCard
-          questionNr={number+1}
-          totalQuestions={TOTAL_QUESTIONS}
-          question={questions[number].question}
-          answers={questions[number].answers}
-          userAnswer={userAnswers? userAnswers[number]: undefined}
-          callback={checkAnswer}
-          /> 
-      )
-    }
+      <Wrapper >
+        <h1 className="quiz__title">MyQuiz</h1>
+        {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
+          <button onClick={startTrivia} className="quiz__start">
+            Start
+          </button>
+        ) : null}
+        {!gameOver ? <p className="quiz__score">Score: {score}</p> : null}
+        {loading && <p className="quiz__loader">Loading Questions ...</p>}
+        {!loading && !gameOver && (
+          <QuestionCard
+            questionNr={number + 1}
+            totalQuestions={TOTAL_QUESTIONS}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswers ? userAnswers[number] : undefined}
+            callback={checkAnswer}
+          />
+        )}
 
-    {
-      !gameOver && !loading && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS -  1 ?
-      ( 
-        <button onClick={nextQuestion} className="quiz__next">Next </button>
-      ):null
-    }
-
+        {!gameOver &&
+        !loading &&
+        userAnswers.length === number + 1 &&
+        number !== TOTAL_QUESTIONS - 1 ? (
+          <button onClick={nextQuestion} className="quiz__next">
+            Next{" "}
+          </button>
+        ) : null}
+      </Wrapper>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/style/App.styles.ts
@@ -0,0 +1,66 @@
+import styled, { createGlobalStyle } from "styled-components";
+import Bg from "../assets/img/bc.jpg";
+
+export const GlobalStyle = createGlobalStyle`
+
+    html{
+        height: 100%;
+    }
+
+    body{
+        background-image: url(${Bg});
+        background-size: cover;
+        margin: 0;
+        padding: 0 20px;
+        display: flex;
+        justify-content: center;
+        position: relative;
+        color: #fff;
+        z-index: -2;
+    }
+    .quiz{
+        position: absolute;
+        top: 0;
+        left:0;
+        width: 100%;
+        min-height: 100vh;
+        z-index: 9;
+        background-color: #00000089;
+    }
+    
+
+    *{ 
+        box-sizing: border-box;
+        font-family: 'Catamaran', sans-serif;
+    }
+
+
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > p {
+    color: #fff;
+  }
+
+  .quiz__score {
+    color: #fff;
+    font-size: 2rem;
+    margin: 0;
+  }
+  .quiz__title {
+    font-family: FasCinate Impact, Haettenschweiler, "Arial Narrow Bold",
+      sans-serif;
+    /* background-image: linear-gradient(180deg, #fff, #87f1ff); */
+    background-size: 100%;
+    background-clip: text;
+    color: #fff;
+    filter: drop-shadow(2px 2px #0085a3);
+    font-size: 70px;
+    text-align: center;
+    margin: 20px;
+  }
+`;
